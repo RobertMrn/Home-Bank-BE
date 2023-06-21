1244ebe4a09fb7ae66904e98ea47359e7cebfe78
robert.marinescu <robert.marin0108@gmail.com>
2023-06-21 17:35:13 +0300

    Finished

---
+++ SpringBootApp/src/main/java/org/DTOs/CreditBureauResponse.java
@@ -0,0 +1,16 @@
+package org.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditBureauResponse {
+    BigDecimal score;
+}
